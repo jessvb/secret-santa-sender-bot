c98973a2ef78a8864f8b5dbd3df2b61e8f0dc9d0
Jessica Van Brummelen <jess.vanbrummelen@gmail.com>
2019-11-18 22:59:18 -0500

    Add couples text file

--- super_santa_bot.py
+++ super_santa_bot.py
@@ -13,8 +13,15 @@ import random
 FILENAME_NAMES_EMAILS = "names_emails.csv"
 FILENAME_EMAIL_OUTLINE = "email.txt"
 FILENAME_HOST_EMAIL_AUTH = "host_email_auth.txt"
-COUPLES = [['jon jessi0', 'elise jessicav'], [
-    'ness jess.vanb mit', 'parv jess csail']]
+FILENAME_COUPLES = "couples.txt"
+
+
+def get_couples(filename):
+    couples = []
+    with open(filename, mode='r', encoding='utf-8') as couples_file:
+        for line in couples_file:
+            couples.append([line.split(",")[0].strip(), line.split(",")[1].strip()])
+    return couples
 
 
 def get_names_emails(filename):
@@ -22,9 +29,9 @@ def get_names_emails(filename):
     emails = []
     with open(filename, mode='r', encoding='utf-8') as csv_file:
         for line in csv_file:
-            names.append(line.split(",")[0])
-            emails.append(line.split(",")[1].split("\n")[0])
-        return names, emails
+            names.append(line.split(",")[0].strip())
+            emails.append(line.split(",")[1].split("\n")[0].strip())
+    return names, emails
 # print(get_names_emails(FILENAME_NAMES_EMAILS))
 
 
@@ -44,15 +51,19 @@ def get_host_email_password(filename):
 # print(get_host_email_password(FILENAME_HOST_EMAIL_AUTH))
 
 # make sure couples aren't paired up
-def is_couple(name1, name2):
+
+
+def is_couple(name1, name2, couples):
     isCoup = False
-    for couple in COUPLES:
+    for couple in couples:
         if(name1 == couple[0] and name2 == couple[1] or name1 == couple[1] and name2 == couple[0]):
             isCoup = True
     return isCoup
 
 # the secret santa selection!
-def make_the_magic(names):
+
+
+def make_the_magic(names, couples):
     done = False
 
     while (done == False):
@@ -64,16 +75,16 @@ def make_the_magic(names):
             giver = names[len(secret_names)]
             # make sure that the giver name != receiver name
             # and that there are no couples paired together
-            while (receiver == giver or is_couple(receiver, giver)):
+            while (receiver == giver or is_couple(receiver, giver, couples)):
                 ind = random.randrange(len(temp_names))
                 receiver = temp_names[ind]
             # add the receiver to the secret_names list
             # and remove the receiver from temp_names
             secret_names.append(temp_names.pop(ind))
-        # if the last two names aren't the same, 
+        # if the last two names aren't the same,
         # and aren't a couple, then we're done!
         # otherwise, restart
-        if (temp_names[0] != names[-1] and not (is_couple(temp_names[0], names[-1]))):
+        if (temp_names[0] != names[-1] and not (is_couple(temp_names[0], names[-1], couples))):
             secret_names.append(temp_names.pop())
             done = True
 
@@ -81,9 +92,10 @@ def make_the_magic(names):
 
 
 def main():
+    couples = get_couples(FILENAME_COUPLES)
     # couples check
     print('couples:')
-    for couple in COUPLES:
+    for couple in couples:
         print(couple)
     y_n = input('these are your couples? (y/n) ')
     if (y_n == 'y' or y_n == 'yes' or y_n == 'Y' or y_n == 'Yes'):
@@ -94,7 +106,7 @@ def main():
         # get the outline/template of the email you want to send
         email_outline = read_email_outline(FILENAME_EMAIL_OUTLINE)
 
-        receiver_names = make_the_magic(names)
+        receiver_names = make_the_magic(names, couples)
         print("ssb made the magic! everyone's paired up :)")
         print("ssb just needs to make its merry way across the interwebs now...")
 
@@ -111,9 +123,9 @@ def main():
             text = text.replace("${REC_NAME}", receiver_name.title())
 
             # set up the rest of the email parameters
-            msg['From']=HOST_EMAIL
-            msg['To']=email
-            msg['Subject']="Super Secret Santa Bot Message for Youuuu!"
+            msg['From'] = HOST_EMAIL
+            msg['To'] = email
+            msg['Subject'] = "Super Secret Santa Bot Message for Youuuu!"
             msg.attach(MIMEText(text, 'plain'))
 
             # SEND IT!
