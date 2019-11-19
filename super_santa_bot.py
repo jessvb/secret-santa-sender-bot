b38bb7103d8cc10be47cf9aaf31d4399d38c41d7
Jessica Van Brummelen <jess.vanbrummelen@gmail.com>
2019-11-18 21:09:36 -0500

    Get the emailing working

--- super_santa_bot.py
+++ super_santa_bot.py
@@ -1,6 +1,12 @@
+########################################################
+### This was made with help from freecodecamp.org <3 ###
+########################################################
+
 # smtplib is for creating an smtp client for emailing :)
 import smtplib
-import email
+# mimetext for emailing
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 FILENAME_NAMES_EMAILS = "names_emails.csv"
 FILENAME_EMAIL_OUTLINE = "email.txt"
@@ -29,12 +35,45 @@ def get_host_email_password(filename):
         email = txt.split('\n')[0]
         password = txt.split('\n')[1]
     return (email, password)
-print(get_host_email_password(FILENAME_HOST_EMAIL_AUTH))
+# print(get_host_email_password(FILENAME_HOST_EMAIL_AUTH))
 
-# get the credentials for the host email address
-(HOST_EMAIL, PASSWORD) = get_host_email_password(FILENAME_HOST_EMAIL_AUTH)
+def main():
+    # get the credentials for the host email address
+    (HOST_EMAIL, PASSWORD) = get_host_email_password(FILENAME_HOST_EMAIL_AUTH)
+    # get the names and email addresses of those you want to send to
+    (names, emails) = get_names_emails(FILENAME_NAMES_EMAILS)
+    # get the outline/template of the email you want to send
+    email_outline = read_email_outline(FILENAME_EMAIL_OUTLINE)
 
-# # set up the smtp client:
-# s = smtplib.SMTP(host='smtp.gmail.com', port=587)
-# s.starttls()
-# s.login(HOST_EMAIL, PASSWORD)
+    # print(email_outline.replace("${REC_NAME}", 'the beautiful rec name'))
+
+    # set up the smtp client
+    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    s.starttls()
+    s.login(HOST_EMAIL, PASSWORD)
+
+    # personalize all the messages per-contact
+    for name, email in zip(names, emails):
+        msg = MIMEMultipart()
+        # substitute the ${GIVE_NAME} and ${REC_NAME} in the text
+        text = email_outline.replace("${GIVE_NAME}", name.title())
+        text = text.replace("${REC_NAME}", "TODO")
+
+        # set up the rest of the email parameters
+        msg['From']=HOST_EMAIL
+        msg['To']=email
+        msg['Subject']="Test email 3"
+        msg.attach(MIMEText(text, 'plain'))
+
+        # SEND IT!
+        s.send_message(msg)
+
+        del msg
+    
+    # quit the smtp session and close the connection
+    s.quit()
+
+    print('MERRY CHRISTMAS! The Secret Santa Bot has done its Super Secret Send!')
+
+if __name__ == '__main__':
+    main()
